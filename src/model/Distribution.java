b2b538d363be0bbe20d719ac6f98d35da84fb9a6
Clarence Soh <clarenceteakang.soh@duke.edu>
2025-11-02 17:42:10 +0000

    Added Distribution interface

--- src/model/Distribution.java
+++ src/model/Distribution.java
@@ -1,1 +1,13 @@
-// Clarence
+/**
+ * @author: Clarence
+ * 
+ * Distribution interface to be implemented by statistical distribution classes, including Pareto.
+ */
+
+package model;
+
+import util.RandomNumberGenerator;
+
+public interface Distribution {
+    double sample(RandomNumberGenerator rng);
+}
