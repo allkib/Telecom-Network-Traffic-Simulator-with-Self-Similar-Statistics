b1f6d7fd48382c65d70a94bbbcabd52a75647102
Clarence Soh <clarenceteakang.soh@duke.edu>
2025-11-02 22:54:49 +0000

    Updated app to use view classes

--- src/app.java
+++ src/app.java
@@ -1,29 +1,7 @@
-import controller.ParameterController;
-import controller.SimulationController;
-import controller.EventController;
-import controller.TrafficController;
-import model.SimulationParameters;
+import view.ConsoleView;
 
 public class app {
     public static void main(String[] args) {
-        ParameterController paramController = new ParameterController();
-
-        SimulationParameters params = SimulationParameters.defaults();
-        paramController.setDefaults(params);
-
-        if (!paramController.validateParameters(params)) {
-            for (String err : paramController.getValidationErrors()) {
-                System.out.println("Parameter error: " + err);
-            }
-            System.out.println("Exiting due to invalid parameters.");
-            return;
-        }
-
-        SimulationController simController = new SimulationController();
-        simController.setTrafficController(new TrafficController());
-        simController.setEventController(new EventController());
-        simController.runSimulation(params);
-
-        System.out.println("Simulation completed. Samples collected: " + simController.getResults().getTimeSeries().size());
+        new ConsoleView().startSimulation(); 
     }
 }
